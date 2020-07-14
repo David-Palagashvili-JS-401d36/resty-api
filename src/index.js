9c64610b25cfa516024c2a80977b1669fc3e7351
Davidoffili <palagashvilid@gmail.com>
2020-07-14 12:42:19 -0700

    added some basic sass to app, header, and footer components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import the central component which sets the root or base of our React App
 import App from './app.js';
 
-// attach component to the DOM
+// attach the component to the DOM
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<App/>, rootElement);
