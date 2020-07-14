9cf7eb80a44c8c6ca8f1b62377ab75d52794be40
Davidoffili <palagashvilid@gmail.com>
2020-07-14 09:56:48 -0700

    added gitignore file - attempting to fix linter issues

--- src/form.js
+++ src/form.js
@@ -6,8 +6,9 @@ class Form extends React.Component {
         this.state = {
             method: '',
             value: '',
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        }
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
 }
