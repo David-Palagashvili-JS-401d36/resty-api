1483e26ab84ffae32d76c9b0e2804cfa679a713b
Davidoffili <palagashvilid@gmail.com>
2020-07-14 09:51:06 -0700

    linter issues persist after linter was installed successfully.

---
+++ src/form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            method: '',
+            value: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+}
