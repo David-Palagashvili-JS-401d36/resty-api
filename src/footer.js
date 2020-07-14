64b7f20be0cfb72afdf0845ee4dcdaf10323e324
Davidoffili <palagashvilid@gmail.com>
2020-07-14 12:04:18 -0700

    added the render method to the Footer component, simple section with id footer and a p tag holding the CF copyright

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,19 @@ import React from 'react';
 import './styles/form.scss';
 
 class Footer extends React.Component {
-
+    constructor() {
+        super();
+        // Each component has it's own state
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <section id='footer'>
+                <p>&copy; CodeFellows 2020</p>
+            </section>
+        );
+    }
 }
 
 export default Footer;
