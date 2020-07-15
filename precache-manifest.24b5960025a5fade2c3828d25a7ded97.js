419620485f057057022b131606134962a6a025df
Davidoffili <runner@fv-az64.xxqrxkedph3ujnkxuh0gee4kbh.bx.internal.cloudapp.net>
2020-07-15 01:25:43 +0000

    Updates

---
+++ precache-manifest.24b5960025a5fade2c3828d25a7ded97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b428f95d9f41971ef2c2e9c22706899",
+    "url": "/resty-api/index.html"
+  },
+  {
+    "revision": "01badc2ff4abd27eee06",
+    "url": "/resty-api/static/css/main.ebe9588e.chunk.css"
+  },
+  {
+    "revision": "ba25565b63430d2febae",
+    "url": "/resty-api/static/js/2.66cb5cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty-api/static/js/2.66cb5cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01badc2ff4abd27eee06",
+    "url": "/resty-api/static/js/main.80ed46f0.chunk.js"
+  },
+  {
+    "revision": "d45ec26b33d5a2923279",
+    "url": "/resty-api/static/js/runtime-main.56ee0e66.js"
+  }
+]);
