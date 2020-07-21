087e4875127b0f1c245329a5351008acf73c1829
Davidoffili <palagashvilid@gmail.com>
2020-07-20 20:16:48 -0700

    updated CR mark in the footer component

--- src/footer.js
+++ src/footer.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 class Footer extends React.Component {
-    // constructor() {
-    //     super();
-    //     // Each component has it's own state
-    //     this.state = {};
-    // }
 
     render() {
         return (
             <section id="footer">
-                <p>&copy; CodeFellows 2020</p>
+                <p>&copy; 2020 David Palagashvili</p>
             </section>
         );
     }
